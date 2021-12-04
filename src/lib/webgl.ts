8fa623148225f423aecb42c6c847a33fcce53bf2
Jeff Schofield <jeff@jeffschofield.com>
2021-12-04 13:26:08 -0500

    refactor: Move to generic shader program factory

--- src/lib/webgl.ts
+++ src/lib/webgl.ts
@@ -1,32 +1,28 @@
-import PassThroughVertexSource from './shaders/triangle.vertex'
+import ScreenCoveringTriangleVertexSource from './shaders/triangle.vertex'
 import { debounceAnimationFrame } from './utils'
 
-export function createFragmentProgram(canvas:HTMLCanvasElement, fragment_source:string) {
+export function createShaderProgram(canvas:HTMLCanvasElement, vertex_source:string, fragment_source:string) {
 	const gl = canvas.getContext("webgl2", { premultipliedAlpha: false })
 	if (!gl) throw new Error('Unable to get WebGL context.')
 
 	const device_pixel_ratio = window.devicePixelRatio || 1
 
-	// Create screen covering triangle
-	let triangle_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, triangle_buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([ -1, 3,  3,-1,  -1,-1 ]), gl.STATIC_DRAW)
-
 	// Create shaders
-	const passthrough_vertex_shader = createShader(gl, gl.VERTEX_SHADER, PassThroughVertexSource) // We aren't doing anything with vertices so this is just a kind of no-op thing
-	const gradient_fragment_shader = createShader(gl, gl.FRAGMENT_SHADER, fragment_source) // This shader actually generates the gradient based on the component curve 
+	const vertex_shader = createShader(gl, gl.VERTEX_SHADER, vertex_source)
+	const fragment_shader = createShader(gl, gl.FRAGMENT_SHADER, fragment_source)
 
 	// Create program
-	const program = createProgram(gl, passthrough_vertex_shader, gradient_fragment_shader)
+	const program = createProgram(gl, vertex_shader, fragment_shader)
+	if (!program) throw new Error('Unable to create shader program')
 
 	// Set up common program uniforms
 	const setResolution = createUniform(gl, program, '2fv', 'resolution')
 
-	// Make sure the gl context is using the gradient program
+	// Make sure the gl context is using the program
 	gl.useProgram(program)
 
 	// Create resize observer on the canvas to re-render it as it moves around
-	const resize_observer = new ResizeObserver((entries) => debounceAnimationFrame(render))
+	const resize_observer = new ResizeObserver(() => debounceAnimationFrame(render))
 	resize_observer.observe(canvas)
 
 	// This function runs the webgl program
@@ -52,10 +48,16 @@ export function createFragmentProgram(canvas:HTMLCanvasElement, fragment_source:
 	}
 
 	return {
+		render,
+		destroy,
 		createUniform: (type:WebGLUniformType, name:string) => createUniform(gl, program, type, name),
-		render: debounceAnimationFrame(render),
-		destroy
+		createAttribute: (name:string) => createAttribute(gl, program, name),
+		setResolution
 	}
+}
+
+export function createFragmentProgram(canvas:HTMLCanvasElement, fragment_source:string) {
+	return createShaderProgram(canvas, ScreenCoveringTriangleVertexSource, fragment_source)
 }
 
 export function createShader(gl:WebGL2RenderingContext, type:number, source:string) {
@@ -101,4 +103,9 @@ export function createUniform(gl:WebGL2RenderingContext, program:WebGLProgram, t
 	return function setUniform(...args) {
 		return gl[`uniform${type}`](location, ...args)
 	}
+}
+
+export function createAttribute(gl:WebGL2RenderingContext, program:WebGLProgram, name:string) {
+	let location = gl.getAttribLocation(program, name)
+	return location
 }
