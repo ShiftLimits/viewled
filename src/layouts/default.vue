c8a8df9b2b483a4b16ad79fb3962b9435d33082f
Jeff Schofield <jeff@jeffschofield.com>
2021-12-04 20:37:13 -0500

    feat: Add device name header to mobile view

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,7 +3,7 @@
 	import Masthead from '../components/Masthead.vue'
 	import SvgIcon from '../components/SvgIcon.vue'
 
-	const { connecting } = useWLEDClient()
+	const { connecting, info } = useWLEDClient()
 </script>
 
 <template>
@@ -12,6 +12,7 @@
 		<div class="text-4xl font-black">Connecting...</div>
 	</div>
 	<template v-else>
+		<div class="lg:hidden p-1/2 bg-gradient-to-br from-neutral-750 to-neutral-850 leading-3/4 font-bold text-lg text-neutral-100">{{ info.name }}</div>
 		<Masthead class="order-7 lg:order-2 z-masthead" />
 		<div class="order-5 flex-1 overflow-y-auto">
 			<router-view />
