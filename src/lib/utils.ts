d8869b47e0f35cdba6c7a4c639c1b0a0df348b97
Jeff Schofield <jeff@jeffschofield.com>
2021-12-07 02:14:21 -0500

    feat: Add `ortho` util to create an ortho matrix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,28 @@ export function hexToBytes(hex:string) {
 	bytes.push(1)
 	return bytes
 }
+
+// Source and credit: https://github.com/toji/gl-matrix/blob/master/src/mat4.js#L1707
+export function ortho(left:number, right:number, bottom:number, top:number, near:number, far:number) {
+	const out = Array.from<number>({length:16})
+  const lr = 1 / (left - right);
+  const bt = 1 / (bottom - top);
+  const nf = 1 / (near - far);
+  out[0] = -2 * lr;
+  out[1] = 0;
+  out[2] = 0;
+  out[3] = 0;
+  out[4] = 0;
+  out[5] = -2 * bt;
+  out[6] = 0;
+  out[7] = 0;
+  out[8] = 0;
+  out[9] = 0;
+  out[10] = 2 * nf;
+  out[11] = 0;
+  out[12] = (left + right) * lr;
+  out[13] = (top + bottom) * bt;
+  out[14] = (far + near) * nf;
+  out[15] = 1;
+  return out;
+}
