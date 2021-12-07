6644d72c4d691320fb1df333d62992478af0af21
Jeff Schofield <jeff@jeffschofield.com>
2021-12-07 02:13:22 -0500

    feat: Add util to convert a hex string to an array of bytes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,10 @@ export function debounceAnimationFrame<F extends (...args:any[]) => any>(fn:F) {
 export function clamp(n:number, min:number, max:number) {
 	return Math.min(max, Math.max(min, n))
 }
+
+export function hexToBytes(hex:string) {
+	const bytes:number[] = []
+	for (let i = 0; i < hex.length; i += 2) bytes.push(parseInt(hex.substr(i, 2), 16) / 255)
+	bytes.push(1)
+	return bytes
+}
