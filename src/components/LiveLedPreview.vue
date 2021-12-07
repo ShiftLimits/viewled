63f70961e55e8fb6d2dca9f2d2de10461c83a2a8
Jeff Schofield <jeff@jeffschofield.com>
2021-12-07 02:21:05 -0500

    fix: Add missing LiveLedPreview component

---
+++ src/components/LiveLedPreview.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+	import { computed, reactive, ref, watch } from 'vue'
+	import { useWLEDClient } from 'vue-wled'
+	import LedPreview from './LedPreview.vue'
+
+	const { onLiveLEDs } = useWLEDClient()
+
+	const leds = reactive<string[]>([])
+	onLiveLEDs(({ leds: new_leds }) => {
+		leds.length = 0
+		leds.push(...new_leds)
+	})
+</script>
+
+<template>
+	<LedPreview :leds="leds" />
+</template>
