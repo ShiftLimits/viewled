3360eed71011d1ea24cc19d4ff6d637360ff8a1c
Jeff Schofield <jeff@jeffschofield.com>
2021-12-09 23:28:31 -0500

    refactor: Make sure number input uses number v-model

--- src/components/NumberInput.vue
+++ src/components/NumberInput.vue
@@ -24,5 +24,5 @@
 </script>
 
 <template>
-	<input type="number" v-model="value" v-bind="$attrs" class="rounded-3/8 text-white bg-neutral-750 px-1/2 py-1/4 leading-3/4 outline-none focus:ring-1/8 min-w-0" />
+	<input type="number" v-model.number="value" v-bind="$attrs" class="rounded-3/8 text-white bg-neutral-750 px-1/2 py-1/4 leading-3/4 outline-none focus:ring-1/8 min-w-0" />
 </template>
