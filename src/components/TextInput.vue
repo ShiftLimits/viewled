6834109148e0d206d7d6ad70844d1ef3d44c777b
Jeff Schofield <jeff@jeffschofield.com>
2021-12-09 23:08:45 -0500

    feat: Add basic text input

---
+++ src/components/TextInput.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<input type="text" v-bind="$attrs" size="1" class="rounded-3/8 text-white bg-neutral-750 px-1/2 py-1/4 leading-3/4 outline-none focus:ring-1/8 min-w-0" />
+</template>
