699c73c27bf7b374b429b5e92bd3725b1d983211
Jeff Schofield <jeff@jeffschofield.com>
2022-06-11 14:07:12 -0400

    feat: Add build step and compression for adding to WLED

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,35 @@
-module.exports = {
-  plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-    'cssnano': {},
-  },
+const { writeFileSync } = require('fs')
+const { basename, resolve } = require("path")
+const cssClassGenerator = require("css-class-generator")
+
+const transformed_names = new Map()
+let index = 0
+
+function generateClassName(name) {
+  if (transformed_names.has(name)) return transformed_names.get(name)
+  else {
+    let transformed_name = cssClassGenerator(index++)
+    transformed_names.set(name, transformed_name)
+    return transformed_name
+  }
 }
+
+const plugins = {
+  'tailwindcss': {},
+  'autoprefixer': {},
+  'cssnano': {},
+}
+
+if (process.env.NODE_ENV == 'production') {
+  plugins['postcss-modules'] = {
+    getJSON(css_file_name, json) {
+      const css_basename = basename(css_file_name, ".css")
+      writeFileSync(resolve(`./dist/${css_basename}-classname-transforms.json`), JSON.stringify(json))
+    },
+    generateScopedName(name, filename, css) {
+      return generateClassName(name)
+    }
+  }
+}
+
+module.exports = { plugins }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import { resolve } from 'path'
 import { colorSuitePlugin } from 'tailwindcss-color-suite'
+import MagicString from 'magic-string';
+import { readFileSync } from 'fs'
+
+let bytes_saved:number = 0
 
 export default defineConfig({
   plugins: [
@@ -12,5 +16,35 @@ export default defineConfig({
       iconDirs: [resolve(process.cwd(), 'src/assets/icons')]
     }),
   ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'postprocess',
+          closeBundle() {
+            console.log(`Saved ${bytes_saved/1000} kB by compressing class names.`)
+          },
+          renderChunk(code, chunk, { sourcemap }) {
+            let src = new MagicString(code)
+
+            const transforms = readFileSync('./dist/main-classname-transforms.json', 'utf8')
+            const replacements = Object.entries<string>(JSON.parse(transforms))
+
+            for (let [key, replace=''] of replacements) {
+              let find = new RegExp(`(?<=^|\\s+|"+|'+|\`+)${key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}(?=\\s+|$|"+|'+|\`+)`, 'g')
+
+              let token:RegExpExecArray
+              while (token = find.exec(code)) {
+                bytes_saved += key.length - replace.length
+                src.overwrite(token.index, token.index + token[0].length, replace)
+              }
+            }
+
+            return code
+          }
+        }
+      ]
+    }
+  },
   envPrefix: 'WLED'
 })
