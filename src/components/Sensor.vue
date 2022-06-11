c7535aea86fc09df55407560bc88a3c62cfecb81
Jeff Schofield <jeff@jeffschofield.com>
2022-06-11 15:28:14 -0400

    feat: Add Sensor component

---
+++ src/components/Sensor.vue
@@ -0,0 +1,11 @@
+﻿<script setup lang="ts">
+	import { onMounted, onUnmounted } from 'vue'
+	const emit = defineEmits(['mounted', 'unmounted'])
+
+	onMounted(() => emit('mounted'))
+	onUnmounted(() => emit('unmounted'))
+</script>
+
+<template>
+
+</template>
